3e2eb6b0d154072b677bc90cabb0e7225b844f07
Amanr4009 <88740658+Amanr4009@users.noreply.github.com>
2024-07-14 14:22:41 +0530

    Update:footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,16 +4,16 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; 2024 Ecommerce. All rights reserved.</p>
+      <p>&copy; 2024 Ecommerce. All rights reserved to Amanr4009.</p>
       <ul>
         <li><a href="#about">About Us</a></li>
         <li><a href="#contact">Contact Us</a></li>
         <li><a href="#terms">Terms & Conditions</a></li>
       </ul>
       <div className="social-media">
-        <a href="#facebook">Facebook</a>
-        <a href="#twitter">Twitter</a>
-        <a href="#instagram">Instagram</a>
+        <a href="https://github.com/Amanr4009">GitHub</a>
+        {/* <a href="#twitter">Twitter</a>
+        <a href="#instagram">Instagram</a> */}
       </div>
     </footer>
   );
